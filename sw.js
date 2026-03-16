daa3a73f4e191b69493634dbdcc683889bb9f92d
louloutheo <louahntheo@gmail.com>
2026-03-16 02:47:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,11 @@
-const CACHE_NAME = 'vn-trip-v4'; // <-- On passe en V4 ici !
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      '/vietnam2026/',
-      '/vietnam2026/index.html',
-      '/vietnam2026/manifest.json'
-    ]))
-  );
+const CACHE = 'vn-2026-v5';
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/vietnam2026/', '/vietnam2026/index.html', '/vietnam2026/manifest.json'])));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    }).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
