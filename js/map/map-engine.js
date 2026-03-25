0d53d4bafb2ac2638e32ed068b58159c99a9048a
louloutheo <louahntheo@gmail.com>
2026-03-25 12:19:29 +0100

    v1

--- js/map/map-engine.js
+++
@@ -1,5 +0,0 @@
-import { initCesiumMap, flyToLocation, flyToUserLocation, resizeCesiumMap } from './map-cesium.js';
-export const initMapEngine = (options = {}) => initCesiumMap(options);
-export const mapFlyToLocation = (location) => flyToLocation(location);
-export const mapFlyToUser = () => flyToUserLocation();
-export const mapResize = () => resizeCesiumMap();
--- js/services/weather.service.js
+++
@@ -1,6 +0,0 @@
-export async function fetchWeatherForDay(day) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${day.lat}&longitude=${day.lon}&daily=temperature_2m_max&timezone=Asia%2FHo_Chi_Minh`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return Math.round(data.daily.temperature_2m_max[0]);
-}
